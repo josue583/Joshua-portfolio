b63df615ce1b409a40ade75db883f82b5a7496a7
ndikumwenayo josue <ndikumwenayojosue@gmail.com>
2025-03-18 21:37:49 +0200

    Fixing path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
    
 
   ],
-  base:process.env.VITE_BASE_PATH || "/Joshua-portfolio",
+  // base:process.env.VITE_BASE_PATH || "/Joshua-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
