2ae6a8a2a5f1293c35bd0160cfba6e471e6d5c70
ndikumwenayo josue <ndikumwenayojosue@gmail.com>
2025-03-18 21:21:41 +0200

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/sidebar.jsx'
 import joshua from '../assets/joshua.JPG'
 import { TypeAnimation } from 'react-type-animation'
 function Home() {
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { Path } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-    // "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    // "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+
+   
 
   ],
+  base:process.env.VITE_BASE_PATH || '/',
+  
   
 })
